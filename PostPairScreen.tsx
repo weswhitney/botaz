8c7090a850f8dc12a87a4173d8abca7be8ffe335
weswhitney <wesleywhitney@gmail.com>
2021-11-30 22:56:05 -0700

    feat: add route for pair post screen

---
+++ PostPairScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const PostPairScreen = () => {
+  return (
+    <View>
+      <Text>pair form</Text>
+    </View >
+  )
+}
+
+export default PostPairScreen
